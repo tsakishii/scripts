0168df11bf3dcec3455dbdf2b9f81546e3ed5a34
tsakishii <ts.chris@protonmail.com>
2025-03-10 10:10:17 +0100

    Update install_my_apps.sh

--- install_my_apps.sh
+++ install_my_apps.sh
@@ -8,37 +8,38 @@ test $? -eq 0 || exit 1 "you should have sudo priveledge to run this script"
 apt-get update
 echo "Installing some applications I like"
 while read -r pkg; do sudo apt-get install -y $pkg ; done < <(cat << EOF
-      alacritty
-      curl
-      fonts-hack-ttf
-      git-all
-      grimshot
-      htop
-      markdown
-      openjdk-21-doc
-      openjdk-21-jdk
-      openjdk-21-jre
-      openjdk-21-source
-      postgresql
-      postgresql-contrib
-      python3-dev
-      python3-pip
-      python3-setuptools
-      sqlite3
-      stow
-      sway
-      sway-backgrounds
-      swaybg
-      swayidle
-      swayimg
-      swaylock
-      texlive
-      texlive-latex-extra
-      vi
-      waybar
-      wget
-      wofi
-      zsh
+alacritty
+curl
+fonts-hack-ttf
+git-all
+gnupg2
+grimshot
+htop
+markdown
+openjdk-21-doc
+openjdk-21-jdk
+openjdk-21-jre
+openjdk-21-source
+postgresql
+postgresql-contrib
+python3-dev
+python3-pip
+python3-setuptools
+sqlite3
+stow
+sway
+sway-backgrounds
+swaybg
+swayidle
+swayimg
+swaylock
+texlive
+texlive-latex-extra
+vi
+waybar
+wget
+wofi
+zsh
 EOF
 )
 echo "press Ctrl+c to quit"
