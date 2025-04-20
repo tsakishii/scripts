a87f713a421c3e3c28731f3b6180e128e8324e45
tsakishii <ts.chris@protonmail.com>
2025-04-20 13:59:07 +0200

    fix: Remove broken function

--- kubectl.sh
+++ kubectl.sh
@@ -11,14 +11,3 @@ else
     echo "Downloading SHA sum to validate binary"
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 fi
-
-echo "Checking sum:"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-STATUS="$(cat kubectl.sha256)  kubectl" | sha256sum --check
-if [[ $STATUS == *"kubectl: OK"* ]]; then
-     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-else
-    echo "Terminating operation"
-    exit 1
-fi
-
